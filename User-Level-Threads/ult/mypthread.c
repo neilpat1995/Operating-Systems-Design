ea17ca90b9ce034fcc90a47ac45d43c9e2a69851
Travis Vanderstad <t.vanderstad@live.com>
2016-02-02 19:39:18 -0500

    filled in mypthread_create()

--- User-Level-Threads/ult/mypthread.c
+++ User-Level-Threads/ult/mypthread.c
@@ -1,10 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "mypthread.h"
 
+// indicates whether mypthreadcreate has beeen called yet
+static int first = 1;
+
+// array of info for all threads
 static mypthread_t threads[MAX_THREADS];
+
+// how many threads have we created?
+static int num_threads = 0;
 
 int mypthread_create(mypthread_t *thread, const mypthread_attr_t *attr,
 			void *(*start_routine) (void *), void *arg)
 {
+	// if we haven't yet called this function, make a struct for the original (main) thread
+	if(!first) {
+		ucontext_t* main_context = (ucontext_t*)malloc(sizeof(ucontext_t));
+		if(getcontext(main_context)){
+			printf("Failed to get main context.\n");
+			return -1;
+		}
+
+		mypthread_t* main_thread = &(threads[num_threads]);
+		main_thread->state = RUNNING;
+		main_thread->id = 0;
+		main_thread->context = main_context;
+		main_thread->join_id = -1;
+
+		first = 0;
+		num_threads++;
+	}
+
+	ucontext_t* new_context = (ucontext_t*)malloc(sizeof(ucontext_t));
+	if(getcontext(new_context)){
+		printf("Failed to get new context.\n");
+		return -1;
+	}
+	makecontext(new_context, start_routine, *(int*)arg);
+
+	mypthread_t* new_thread = &(threads[num_threads]);
+	new_thread->state = READY;
+	new_thread->id = num_threads;
+	new_thread->context = new_context;
+	new_thread->join_id = -1;
+
+	num_threads++;
 	return 0;
 }
 
