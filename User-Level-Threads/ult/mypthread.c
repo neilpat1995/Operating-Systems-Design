2f1d6e2245c0eb8920f52c2b45ce5a06af5b9065
Travis Vanderstad <t.vanderstad@live.com>
2016-02-06 22:32:20 -0500

    fewer print statements

--- User-Level-Threads/ult/mypthread.c
+++ User-Level-Threads/ult/mypthread.c
@@ -78,20 +78,17 @@ int get_next_thread_id() {
 
 		// if we got to the end of the list
 		if(next_thread_id == num_threads) {
-			printf("[loop to thread array start]\n");
 			// go back to the beginning
 			next_thread_id = 0;
 		}
 
 		// if we wrapped all the way back to where we started
 		if(next_thread_id == running_thread_id) {
-			printf("[next thread is running thread]\n");
 			// well, looks like this one's all we got
 			break;
 		}
 	} while(threads[next_thread_id].state != READY && threads[next_thread_id].state != NEW);
 
-	printf("[next thread: %d]\n", next_thread_id);
 	return next_thread_id;
 }
 
